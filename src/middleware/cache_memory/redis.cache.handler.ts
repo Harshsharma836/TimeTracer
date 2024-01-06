b7e48b12c272e2f775ef449ff4aff7516774d9de
Harsh Sharma <Harshsharma1421@gmail.com>
2024-01-06 23:46:39 +0530

    update-redis-cache-memeory

--- src/middleware/cache_memory/redis.cache.handler.ts
+++ src/middleware/cache_memory/redis.cache.handler.ts
@@ -1,21 +1,43 @@
+import { logger } from "../../middleware/log.middleware";
 import clientRedis from "../../redis/redis.config";
 
+const USER_KEY_PREFIX = "user";
+
 export const cacheStore = async (userId: number, task: string) => {
-  // Store key as user:id value as json
-  await clientRedis.lPush(`user:${userId}`, JSON.stringify(task));
-  // Expire Key after 5min , so we take the fresh data
-  await clientRedis.EXPIRE(`user:${userId}`, 300);
+  const userKey = `${USER_KEY_PREFIX}:${userId}`;
+  
+  try {
+    // Store key as user:id value as json
+    await clientRedis.lPush(userKey, JSON.stringify(task));
+    // Expire Key after 5min , so we take the fresh data
+    await clientRedis.expire(userKey, 300);
+  } catch (error) {
+    console.error(`Error caching data for user ${userId}:`, error);
+    // Handle the error as per your application's needs
+  }
 };
 
 export const cacheExpire = async (userId: number) => {
-  await clientRedis.EXPIRE(`user:${userId}`, 0);
+  const userKey = `${USER_KEY_PREFIX}:${userId}`;
+  
+  try {
+    await clientRedis.expire(userKey, 0);
+  } catch (error) {
+    console.error(`Error expiring cache for user ${userId}:`, error);
+    // Handle the error as per your application's needs
+  }
 };
 
-// Not using Now : Bull MQ is using
+// Not using Now: Bull MQ is using
 // Queue for Multiple Request handling for Task Completed
 export const requestHandler = async (userId: number, taskId: number) => {
-  await clientRedis.lPush(
-    `Request-Task-Completed`,
-    `user:${userId} task:${taskId}`,
-  );
+  try {
+    await clientRedis.lPush(
+      `Request-Task-Completed`,
+      `user:${userId} task:${taskId}`
+    );
+  } catch (error) {
+    logger.error(`Error in Redis Cache Memory ${error}`);
+    console.error(`Error handling request for user ${userId}, task ${taskId}:`, error);
+  }
 };
--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,7 +1,14 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
-export const errorMiddleWare = (req: Request, res: Response) => {
-  res.send({
-    msg: "Something Wrong",
+export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+
+  // Log the error for debugging purposes
+  console.error(err);
+
+  // Send a proper JSON response with a status code and error message
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message || "Something went wrong",
   });
 };
